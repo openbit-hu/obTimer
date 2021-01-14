1336a6c2fac31f7c8d5821470576203070db46b5
openbit-hu <72387216+openbit-hu@users.noreply.github.com>
2021-01-14 23:11:25 +0100

    main.ts, test.ts frissítése

--- main.ts
+++ main.ts
@@ -1,15 +1,22 @@
 namespace obTimer {
-    let dt:number=1000
-    let res:number=200
+    let dt:number=1000000
+    let res:number=200000
     let t0:number
     let timerCallback:()=>void
-    export function init(){
+    //% blockId="obTimer_init"
+    //% block="start Timer event || in $time || with $resolution"
+    export function init(time?:number,resolution?:number){
+        if(time)dt=time*1000
+        if(resolution)res=resolution*1000
         t0=control.micros()
         control.inBackground(function () {
-            control.waitMicros(res*1000)
-            let t1=control.micros()
-            if(t1-t0>dt*1000){
-                timerCallback()
+            while(true){
+                control.waitMicros(res)
+                let t1=control.micros()
+                if(t1-t0>dt){
+                    timerCallback()
+                    t0=t1
+                }
             }
         })
     }
@@ -22,5 +29,4 @@ namespace obTimer {
     export function onTimerEvent(body: () => void): void {
         timerCallback=body
     }
-
 }
--- test.ts
+++ test.ts
@@ -1,1 +1,8 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+let x=0
+
+obTimer.onTimerEvent(function () {
+  led.plot(x++, 0)
+})
+
+obTimer.init()
